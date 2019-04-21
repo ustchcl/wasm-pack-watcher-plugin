d22697bfe0a5e020519f7bd5ef79db54c00df599
KoishiChan <qwq@koishi.top>
2019-04-22 01:19:32 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const process = require('process')
 const validateOptions = require('schema-utils')
+const watch = require('node-watch')
 const cp = require('child_process')
 
 const Plugin = 'WasmPackWatcherPlugin'
@@ -17,19 +18,8 @@ class WasmPackWatcherPlugin {
 		validateOptions(require('./options.json'), options, Plugin)
 		this.options = options
 	}
-	apply(compiler) {
-		compiler.hooks.afterCompile.tap(Plugin, compilation => {
-			const dir = path.resolve(this.options.crateRoot, 'pkg')
-			const pkgs = new RegExp('^' + dir + '/.*' + '$')
-			const rm = set => {
-				set.forEach(e => { if (pkgs.test(e)) set.delete(e) })
-			}
-			compilation.contextDependencies.delete(dir)
-			rm(compilation.fileDependencies)
-			rm(compilation.contextDependencies)
-			compilation.contextDependencies.add(this.options.sourceRoot)
-		})
-		compiler.hooks.compile.tap(Plugin, compilation => {
+	apply() {
+		watch(this.options.sourceRoot, { recursive: true }, (evt, name) => {
 			const { status: err, pid } = cp.spawnSync('wasm-pack', ['build', '--' + this.options.mode], {
 				cwd: this.options.crateRoot,
 				stdio: "inherit"
